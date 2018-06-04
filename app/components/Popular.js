260bbbaa4d492a95e8b87074439bd8d0ef7bfaf0
Anna Spysz <anna@annaspysz.com>
2018-06-04 16:50:39 -0700

    Styling

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,8 +3,18 @@ const React = require('react');
 
 class Popular extends React.Component {
 	render() {
+		const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
+
 		return (
-			<div>Popular!</div>    
+			<ul className="languages">
+				{language.map(function(lang) {
+					return (
+						<li>
+							{lang}
+						</li>
+					)
+				})}
+			</ul>
 		)
 	}
 }
